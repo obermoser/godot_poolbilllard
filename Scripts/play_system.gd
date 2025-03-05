767e1bfc33a79981415ddbeb20bf8204c9d12375
obermoser <bernhard.obermoser@icloud.com>
2025-03-05 18:55:36 +0100

    Cue Stick & AimCam

--- Scripts/play_system.gd
+++ Scripts/play_system.gd
@@ -1,3 +1,12 @@
 #By Bernhard Obermoser 2025
 #Main Playsystem
 extends Node3D
+
+@export var _cue_ball : RigidBody3D
+
+
+func _process(delta: float) -> void:
+	#Shooting the ball
+	if Input.is_action_just_pressed("shoot"):
+		var impulse_vector = Vector3(1,0,0)
+		_cue_ball.apply_central_impulse(impulse_vector)
