2a1befdd76c686c76b3b67da7805cccdecfc9a95
obermoser <bernhard.obermoser@icloud.com>
2025-03-09 17:49:55 +0100

    Shoot according to direction and force

--- Scripts/play_system.gd
+++ Scripts/play_system.gd
@@ -9,13 +9,15 @@ extends Node3D
 @export var _cue_stick : Node3D
 
 @export_category("Cue Stick Settings")
-@export var _stick_min_z : float = 0.75
-@export var _stick_max_z : float = 1.40
-@export var _stick_sensitivity :float = 0.01
+@export var _stick_min_z := 0.75
+@export var _stick_max_z := 1.40
+@export var _stick_sensitivity := 0.01
 @onready var _stick_animation_player: AnimationPlayer = $AnimationPlayer
 @export_subgroup("Shot Power")
-@export var _shot_power_min = 0.5
-@export var _shot_power_max = 2.5
+@export var _shot_power_min := 0.5
+@export var _shot_power_max := 5.0
+
+var _shot_percent:float = 0.0
 
 
 func _ready() -> void:
@@ -33,18 +35,28 @@ func _input(event: InputEvent) -> void:
 	if mouse_motion:
 		_aim_container.rotation_degrees.y += mouse_motion.relative.x
 		
+		#Setting Shot Percent and move the stick forward or backward
+		_shot_percent += mouse_motion.relative.y * _stick_sensitivity
+		_shot_percent = clamp(_shot_percent, 0,1)
+		_cue_stick.position.z = lerp(_stick_min_z, _stick_max_z, _shot_percent)
+		
 		#Moving the Cue Stick forward/backward
-		_cue_stick.position.z += mouse_motion.relative.y * _stick_sensitivity
-		_cue_stick.position.z = clamp(_cue_stick.position.z, _stick_min_z,_stick_max_z )
+		#_cue_stick.position.z += mouse_motion.relative.y * _stick_sensitivity
+		#_cue_stick.position.z = clamp(_cue_stick.position.z, _stick_min_z,_stick_max_z )
 
 func _handle_shot_input():
 	#Shooting the ball
 	if Input.is_action_just_pressed("shoot"):
 		_stick_animation_player.play("shoot_stick")
-		
 
 func _shoot_ball():
 	#Shooting the ball
+	var _shot_power := lerp(_shot_power_min, _shot_power_max, _shot_percent) as float
 	var stick_direction = -_aim_container.basis.z
-	_cue_ball.apply_central_impulse(stick_direction)
+	var _shot_vector = stick_direction * _shot_power
+	
+	_cue_ball.apply_central_impulse(_shot_vector)
+	#Hide the stick while shot is processing
+	_cue_stick.visible = false
+	
 	GameEvents.cue_ball_hit.emit()
