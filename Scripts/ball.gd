f9611a0a34eadfc13d700d9b7810f1c89484ff8c
obermoser <bernhard.obermoser@icloud.com>
2025-03-13 22:23:19 +0100

    Ball Rack Done

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -5,13 +5,21 @@ extends RigidBody3D
 const BALL_RADIUS := 0.027
 const BALL_DIAMETER := BALL_RADIUS * 2
 
-@export var _texture : Texture2D
+var _ball_num := 0
+var _texture : Texture2D
+var _ball_type : Enums.BallType
 
 func _ready() -> void:
 	_apply_new_material()
 
-
 func setup_ball(ball_num:int, ball_x:float, ball_z:float)->void:
+	_ball_num = ball_num
+	
+	if _ball_num == 8:
+		_ball_type = Enums.BallType.EIGHT_BALL
+	elif ball_num == 0:
+		_ball_type = Enums.BallType.CUE_BALL
+	
 	self.position.x = ball_x
 	self.position.z = ball_z
 	
---
+++ Scripts/enums.gd
@@ -0,0 +1,9 @@
+extends Node
+
+enum BallType{
+	SOLIDS,
+	STRIPES,
+	CUE_BALL,
+	EIGHT_BALL
+}
+ 
